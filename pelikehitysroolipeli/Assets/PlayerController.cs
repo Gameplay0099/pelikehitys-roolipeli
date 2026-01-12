19369b861480e14c3a0403f28fc5abada3e06dd6
Tia Kiesiläinen <116544056+kieti-bc@users.noreply.github.com>
2026-01-12 15:45:25 +0200

    Added tiles
    Created Prefabs for Door and Merchants

---
+++ pelikehitysroolipeli/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 lastMovement;
+    Rigidbody2D rb;
+    [SerializeField]
+    float moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lastMovement = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + lastMovement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Huomaa mitä pelaaja löytää
+        if (collision.CompareTag("Door"))
+        {
+            Debug.Log("Found Door");
+        }
+        else if (collision.CompareTag("Merchant"))
+        {
+            Debug.Log("Found Merchant");
+        }
+    }
+
+    void OnMoveAction(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+        lastMovement = v;
+    }    
+}
